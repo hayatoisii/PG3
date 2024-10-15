7f104f3e270d3860ff4ec60cbd6451377a635ca7
hayatoisii <k023g0022@g.neec.ac.jp>
2024-10-15 12:58:08 +0900

    完

--- main.cpp
+++ main.cpp
@@ -7,12 +7,23 @@
 		return (Recursive(n - 1) * 2 - 50);
 	}
 
+	int Recursive2(int n) {
+		if (n <= 1) {
+			return (100);
+		}
+		return n * 100;
+	}
+
 	int main() {
-		int n = 10;
-		int result;
+		int n = 4;
+		int result{};
+		int result2{};
 
 		result = Recursive(n);
+		result2 = Recursive2(n);
+
 		printf("%d時間 = %d\n", n, result);
+		printf("%d時間 = %d\n", n, result2);
 
 		return (0);
 	}
